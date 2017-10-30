9edaadd4029a6cd10b9eee5c7c67756a5e93570f
Alessandro <ale-remo@hotmail.com>
2017-10-30 17:32:39 -0300

    Preenchimento utilizando floodFill concluído

--- js/paint.js
+++ js/paint.js
@@ -5,14 +5,12 @@ class InterfaceGrafica {
         this.numeroQuadradosPorLinha = numeroQuadradosPorLinha;
         this.frameBuffer = new FrameBuffer(numeroQuadradosPorLinha);
         this.canvasDiv = canvasDiv;
-
+        this.telaAtiva = "";
         this.desenharCanvasInicial()
     }
 
-    desenharPonto(x, y) {
-    	if (x >= 0 && y >= 0) {
-        	this.frameBuffer.setPixel(x, y, "#64b5f6")
-    	}
+    desenharPonto(x, y, color = "#64b5f6") {
+        this.frameBuffer.setPixel(x, y, color)
     }
 
     desenharCanvasInicial() {
@@ -34,7 +32,7 @@ class InterfaceGrafica {
         }
     }
 
-    desenharLinhaBresenham(x0, y0, x1, y1) {
+    desenharLinhaBresenham(x0, y0, x1, y1, color) {
         x0 = Number(x0);
         y0 = Number(y0);
         x1 = Number(x1);
@@ -101,7 +99,7 @@ class InterfaceGrafica {
         // ------------- REFLEXÃO -1
 
         if (trocay) {
-            for (var i = 0 ; i < pixels.length; i++) {
+            for (var i = 0; i < pixels.length; i++) {
                 var [elemento_x, elemento_y] = pixels[i];
 
                 pixels[i] = [elemento_x, -elemento_y];
@@ -109,7 +107,7 @@ class InterfaceGrafica {
         }
 
         if (trocax) {
-            for (var i = 0 ; i < pixels.length; i++) {
+            for (var i = 0; i < pixels.length; i++) {
                 var [elemento_x, elemento_y] = pixels[i];
 
                 pixels[i] = [-elemento_x, elemento_y];
@@ -117,15 +115,93 @@ class InterfaceGrafica {
         }
 
         if (trocaxy) {
-            for (var i = 0 ; i < pixels.length; i++) {
+            for (var i = 0; i < pixels.length; i++) {
                 var [elemento_x, elemento_y] = pixels[i];
 
                 pixels[i] = [elemento_y, elemento_x];
             }
         }
 
-        for (var i = 0 ; i < pixels.length; i++) {
+        for (var i = 0; i < pixels.length; i++) {
             var [elemento_x, elemento_y] = pixels[i];
+
+            this.desenharPonto(elemento_x, elemento_y, color);
+        }
+
+        this.draw();
+    }
+
+    desenharCirculo(inputX, inputY, raio) {
+        inputX = Number(inputX);
+        inputY = Number(inputY);
+        raio = Number(raio);
+
+        // Desloca o centro para (0, 0)
+        var x, y;
+
+        var pixelsPrimeiroOctante = [];
+
+        x = 0;
+
+        y = raio;
+
+        var p = 1 - raio;
+
+        pixelsPrimeiroOctante.push([x, y]);
+
+        // this.desenharPonto(x, y);
+
+        while (x < y) {
+            x++;
+
+            if (p < 0) {
+                p += (2 * x) + 3
+            } else {
+                y--;
+                p += (2 * x) - (2 * y) + 5
+            }
+
+            // this.desenharPonto(x, y);
+
+            pixelsPrimeiroOctante.push([x, y]);
+        }
+
+        var pixelsToDraw = pixelsPrimeiroOctante.slice();
+
+        for (var i = 0; i < pixelsPrimeiroOctante.length; i++) {
+            var pixelX = pixelsPrimeiroOctante[i][0];
+            var pixelY = pixelsPrimeiroOctante[i][1];
+
+            pixelsToDraw.push([pixelX, pixelY]);
+
+            // -x y
+            pixelsToDraw.push([-pixelX, pixelY]);
+
+            // x -y
+            pixelsToDraw.push([pixelX, -pixelY]);
+
+            // -x -y
+            pixelsToDraw.push([-pixelX, -pixelY]);
+
+
+            // y x
+            pixelsToDraw.push([pixelY, pixelX]);
+
+            // -y x
+            pixelsToDraw.push([-pixelY, pixelX]);
+
+            // Y -x
+            pixelsToDraw.push([pixelY, -pixelX]);
+
+            // -y -x
+            pixelsToDraw.push([-pixelY, -pixelX]);
+        }
+
+        for (var i = 0; i < pixelsToDraw.length; i++) {
+            var [elemento_x, elemento_y] = pixelsToDraw[i];
+
+            elemento_x += inputX;
+            elemento_y += inputY;
 
             this.desenharPonto(elemento_x, elemento_y);
         }
@@ -133,96 +209,34 @@ class InterfaceGrafica {
         this.draw();
     }
 
-    desenharCirculo(inputX, inputY, raio) {
-    	console.log('desenharCirculo');
+    preencher(x, y, color, edgeColor = "#64b5f6") {
+        this.floodFill(x, y, color, edgeColor);
 
-    	inputX = Number(inputX);
-    	inputY = Number(inputY);
-    	raio = Number(raio);
-
-    	// Desloca o centro para (0, 0)
-    	var x, y;
-
-    	var pixelsToDraw = [];
-    	var pixelsPrimeiroOctante = []
-
-    	x = 0;
-
-    	y = raio;
-
-    	var p = 1 - raio;
-
-    	pixelsPrimeiroOctante.push([x, y]);
-
-    	// this.desenharPonto(x, y);
-
-    	while (x < y) {
-    		x++;
-
-    		if (p < 0) {
-    			p += (2*x) + 3
-    		} else {
-    			y--;
-    			p += (2*x) - (2*y) + 5 
-    		}
-
-    		// this.desenharPonto(x, y);
-
-    		pixelsPrimeiroOctante.push([x, y]);
-
-    	}
-
-    	var pixelsToDraw = pixelsPrimeiroOctante.slice();
-
-    	for (var i = 0; i < pixelsPrimeiroOctante.length; i++) {
-    		var pixelX = pixelsPrimeiroOctante[i][0];
-    		var pixelY = pixelsPrimeiroOctante[i][1];
-
-    		pixelsToDraw.push([pixelX, pixelY]);
-
-    		// -x y
-    		pixelsToDraw.push([-pixelX, pixelY]);
-
-    		// x -y
-    		pixelsToDraw.push([pixelX, -pixelY]);
-
-    		// -x -y
-    		pixelsToDraw.push([-pixelX, -pixelY]);
-
-
-			// y x
-    		pixelsToDraw.push([pixelY, pixelX]);
-
-			// -y x
-    		pixelsToDraw.push([-pixelY, pixelX]);
-
-			// Y -x
-    		pixelsToDraw.push([pixelY, -pixelX]);
-
-			// -y -x
-    		pixelsToDraw.push([-pixelY, -pixelX]);
-    	}
-
-    	for (var i = 0; i < pixelsToDraw.length; i++) {
-    		var [elemento_x, elemento_y] = pixelsToDraw[i];
-
-    		elemento_x += inputX;
-    		elemento_y += inputY;
-
-            this.desenharPonto(elemento_x, elemento_y);
-    	}
-
-    	this.draw();
+        this.draw();
     }
 
+    floodFill(x, y, color, edgeColor = "#64b5f6") {
+        x = Number(x);
+        y = Number(y);
+
+        var current = this.frameBuffer.getPixel(x, y);
+
+        if (current && current != edgeColor && current != color) {
+            this.desenharPonto(x, y, color);
+            this.floodFill(x + 1, y, color, edgeColor);
+            this.floodFill(x, y + 1, color, edgeColor);
+            this.floodFill(x - 1, y, color, edgeColor);
+            this.floodFill(x, y - 1, color, edgeColor);
+        }
+    }
 
     draw() {
         for (var i = 0; i < this.numeroQuadradosPorLinha; i++) {
             for (var j = 0; j < this.numeroQuadradosPorLinha; j++) {
                 // var pixel = document.getElementById(i + "_" + j);
                 $("#" + i + "_" + j).animate({
-				    backgroundColor: this.frameBuffer.getPixel(i, j)
-				  }, 1500);
+                    backgroundColor: this.frameBuffer.getPixel(i, j)
+                }, 200);
 
                 // pixel.style.backgroundColor = this.frameBuffer.getPixel(i, j)
             }
@@ -233,6 +247,11 @@ class InterfaceGrafica {
         this.frameBuffer.reset();
 
         this.draw();
+    }
+
+    ativarTela(tela) {
+        this.telaAtiva = tela;
+        console.log(this.telaAtiva);
     }
 }
 
@@ -256,12 +275,22 @@ class FrameBuffer {
         }
     }
 
+    isValidPixel(x, y) {
+        return x >= 0 && x < this.matriz.length && y >= 0 && y < this.matriz.length
+    }
+
     setPixel(x, y, cor) {
-        this.matriz[x][y] = cor;
+        if (this.isValidPixel(x, y)) {
+            this.matriz[x][y] = cor;
+        }
     }
 
     getPixel(x, y) {
-        return this.matriz[x][y];
+        if (this.isValidPixel(x, y)) {
+            return this.matriz[x][y];
+        } else {
+            return null
+        }
     }
 
     reset() {
@@ -271,9 +300,40 @@ class FrameBuffer {
 
 var interfaceGrafica;
 
-$(function() {
-    var numeroQuadradosPorLinha = 15;
+$(function () {
+    var numeroQuadradosPorLinha = 40;
     interfaceGrafica = new InterfaceGrafica(numeroQuadradosPorLinha, $(".grid"));
+
+    var pixelsSelecionados = [];
+
+    $(".pixel").on('click', function (event) {
+        var [x, y] = event.target.id.split('_');
+
+        if (interfaceGrafica.telaAtiva == "preenchimento") {
+            var color = "#" + $("#color").val();
+
+            interfaceGrafica.preencher(x, y, color)
+        } else if (interfaceGrafica.telaAtiva == "bresenham") {
+            if (pixelsSelecionados.length >= 2) {
+                pixelsSelecionados.shift();
+            }
+
+            pixelsSelecionados.push({x: x, y: y});
+
+            if (pixelsSelecionados.length == 2) {
+                interfaceGrafica.desenharLinhaBresenham(pixelsSelecionados[0].x, pixelsSelecionados[0].y,
+                    pixelsSelecionados[1].x, pixelsSelecionados[1].y)
+            }
+        } else if (interfaceGrafica.telaAtiva == "circulo") {
+            var raio = $('#raio_circulo').val();
+
+            if (!raio) {
+                alert('Insira um raio válido');
+            }
+
+            interfaceGrafica.desenharCirculo(x, y, raio);
+        }
+    })
 });
 
 
